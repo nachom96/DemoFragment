75474c044b1f2a76e3eae37e68d3d49440c92247
Nacho <nacho_085@hotmail.com>
2022-03-01 10:49:12 +0100

    Initial commit

---
+++ app/src/main/java/com/example/demofragment/Option1Fragment.kt
@@ -0,0 +1,11 @@
+package com.example.demofragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class Option1Fragment : Fragment(R.layout.option1_fragment) {
+
+}
